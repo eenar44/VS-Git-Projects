a8e740dad1ea9c18e28cb75db2aae6163fc77721
eenar44 <eenarizwan@gmail.com>
2025-02-17 22:29:49 +0000

    Created a basic rocket simulator, this one can only go up and down, students can change major variables like thrust etc

---
+++ Rocket Sim/student.py
@@ -0,0 +1,8 @@
+# 🚀 Student-editable settings
+GRAVITY = 0.5   # Gravity pulling rocket down
+THRUST = 1.2    # How powerful the rocket's engine is
+
+# Example: Students can write a simple autopilot function
+def autopilot(altitude, velocity):
+    """Simple autopilot: Fires thrust if altitude is low."""
+    pass
